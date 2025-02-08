c96251ae92fa4336dcd9c05a05c377f0dd73b137
aseemkamra <aseem0868.becse24@chitkara.edu.in>
2025-02-08 14:37:00 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -16,7 +16,10 @@ int main() {
     else if (a == 'D') {
         printf("Below Average");
     } 
-    else {
+    else if(a == 'F'){
+        printf("Fail");
+    } 
+    else{
         printf("Invalid grade");
     }
 
